3caac6723d90274ffd5b12559f516fa3003fc398
ElectroScience <46717842+ElectroSciences@users.noreply.github.com>
2020-02-21 21:23:42 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long int fact (int n){
+
+    unsigned long long result =1;
+
+    if(n==0)
+       return n+1;
+
+    for(int i=1; i<=n; i++){
+
+       result *= i;
+    }
+
+    return result;
+
+}
+
+int main()
+{
+    int numb=0;
+
+    printf("which factorial do you want to know ?\n");
+    scanf("%d", &numb);
+    int result=fact(numb);
+    printf("The result of the operation is %d\n", result);
+
+}
